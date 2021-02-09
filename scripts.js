b279d8e0c7f614792500deb0db9d1c8ffcc52b16
uetiari <arianeueti@gmail.com>
2021-02-09 17:10:11 -0300

    Correção no cálculo das transaçõesas entradas

--- scripts.js
+++ scripts.js
@@ -107,9 +107,9 @@ const DOM = {
 
 const Utils = {
     formatAmount(value){
-        value = Number(value) * 100
+        value = value * 100
         //value = Number(value.replace(/\,\./g, ""))*100
-        return value
+        return Math.round(value)
     },
 
     formatDate(date){
